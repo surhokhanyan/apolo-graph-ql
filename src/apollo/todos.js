4b3f70ef7e37f978307f307ed40ada9cbe0d03c5
simonyan95 <simonyan.ars95@gmail.com>
2023-07-14 11:26:32 +0400

    Apolo

---
+++ src/apollo/todos.js
@@ -0,0 +1,40 @@
+import {gql} from "@apollo/client";
+
+export const ALL_TODO = gql`
+    query ALLTodos {
+      todos: allTodos{
+        id
+        title
+        completed
+      }
+    }
+`;
+
+
+export const ADD_TODO = gql`
+    mutation AddTodo ($title: String!, $userId: ID!, $completed: Boolean!){
+      newTodo: createTodo (user_id: $userId, title: $title, completed: $completed){
+        id
+        title
+        completed
+      }
+    }
+`
+
+export const UPDATE_TODO = gql`
+    mutation UpdateTodo($id: ID!, $completed: Boolean) {
+      updateTodo(id: $id, completed: $completed){
+        id
+        title
+        completed
+      }
+    }
+`
+
+export const DELETE_TODO = gql`
+    mutation DeleteTodo($id: ID!){
+      removeTodo(id: $id){
+        id
+      }
+    }
+`
